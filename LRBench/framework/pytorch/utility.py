8870808e781ffc06f2f7c8a07637e69da8b59a8e
Yanzhao Wu <yanzhaowu@gatech.edu>
2022-01-29 20:31:26 -0500

    add utility functions for PyTorch

--- LRBench/framework/pytorch/utility.py
+++ LRBench/framework/pytorch/utility.py
@@ -1,9 +1,38 @@
 #!/bin/python
 
 """
-Another option is to use torch.optim.lr_scheduler.LambdaLR
+utility functions for PyTorch
 """
 
+import shutil
+import torch
+
+
+# another option is to use torch.optim.lr_scheduler.LambdaLR
 def update_learning_rate(optimizer, lr):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
+        
+
+# classification evaluation Top-k accuracy
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+    
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth'):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'model_best.pth')
--- env.sh
+++ env.sh
@@ -1,3 +1,6 @@
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
 export PYTHONPATH=$PYTHONPATH:`pwd`
 export DJANGO_SETTINGS_MODULE=LRBenchDjango.settings
+
+# avoid committing the settings.py
+# git update-index --assume-unchanged  LRBenchDjango/settings.py
