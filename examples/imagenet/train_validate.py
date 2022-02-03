1db2d99b6da07a4ffe671144dfa9984107c362e1
Yanzhao Wu <yanzhaowumail@gmail.com>
2022-02-03 13:59:25 -0500

    full version

--- examples/imagenet/train_validate.py
+++ examples/imagenet/train_validate.py
@@ -1,6 +1,9 @@
 import torch
 import time
 
+
+from LRBench.framework.pytorch.utility import update_learning_rate
+
 from LRBench.monitor.meters import (
     Summary,
     AverageMeter,
@@ -10,8 +13,6 @@ from LRBench.monitor.meters import (
 from LRBench.framework.pytorch.utility import (
     accuracy
 )
-
-
 
 def validate(val_loader, model, criterion, args):
     batch_time = AverageMeter('Time', ':6.3f', Summary.NONE)
@@ -54,3 +55,57 @@ def validate(val_loader, model, criterion, args):
         progress.display_summary()
 
     return top1.avg
+
+
+def train(train_loader, model, criterion, optimizer, epoch, args, lrbenchLR=None):
+    batch_time = AverageMeter('Time', ':6.3f')
+    data_time = AverageMeter('Data', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e', store=True)
+    top1 = AverageMeter('Acc@1', ':6.2f', store=True)
+    top5 = AverageMeter('Acc@5', ':6.2f', store=True)
+    progress = ProgressMeter(
+        len(train_loader),
+        [batch_time, data_time, losses, top1, top5],
+        prefix="Epoch: [{}]".format(epoch))
+
+    # switch to train mode
+    base_iter = epoch * len(train_loader)
+    model.train()
+
+    end = time.time()
+    for i, (images, target) in enumerate(train_loader):
+        # LRBench
+        if lrbenchLR:
+            cur_iter = base_iter + i
+            update_learning_rate(optimizer, lrbenchLR.getLR(cur_iter))
+            #print("learning rate: ", cur_iter, lrbenchLR.getLR(cur_iter))
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        if args.gpu is not None:
+            images = images.cuda(args.gpu, non_blocking=True)
+        if torch.cuda.is_available():
+            target = target.cuda(args.gpu, non_blocking=True)
+
+        # compute output
+        output = model(images)
+        loss = criterion(output, target)
+
+        # measure accuracy and record loss
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        losses.update(loss.item(), images.size(0))
+        top1.update(acc1[0], images.size(0))
+        top5.update(acc5[0], images.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            progress.display(i)
+    return losses, top1, top5
